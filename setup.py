62867029a3c3c1e9d16f410295407e7bd6327c80
PxielDirt <103628770+PxielDirt@users.noreply.github.com>
2024-02-14 20:44:57 +0000

    hi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ERLC",                 
-    version="1.2.1",                     
+    version="1.2.2",                     
     author="mark.api",                    
     description="An API wrapper for the ERLC API",
     long_description=long_description,      
